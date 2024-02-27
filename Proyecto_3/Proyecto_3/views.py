37b9da8e769a4dee395d29be1e7643ea40ac9556
Alejandra Castellanos <alejac1902@gmail.com>
2024-02-26 20:56:32 -0500

    Continuación Proyecto 3

---
+++ Proyecto_3/Proyecto_3/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def formulario(request):
+  context = {
+    
+  
+  }
+  return render (request, 'index.html', context)
---
+++ Proyecto_3/static/main.js
@@ -0,0 +1,12 @@
+const num_ficha = document.getElementById("num_ficha")
+
+
+//Validación Número de ficha cod_*****//
+
+const validar = function () {
+  let reg = /^[a-zA-Z][a-zA-Z0-9]
+
+}
+
+
+
